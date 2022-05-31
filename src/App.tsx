5359b4392031c42adfb963dec328bd31530faa16
humonnom <josojoso0702@gmail.com>
2022-05-31 18:04:52 +0900

    Use react-router-dom for pagenation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
+  const handleClick = () => {};
   return (
     <div className='App'>
-      <button>회원가입</button>
+      <button type='button' onClick={handleClick}>
+        <Link to='/join'>Join</Link>
+      </button>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/join' element={<p>join</p>} />
+      <Route path='/confirm' element={<p>confirm</p>} />
+      <Route path='/' element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
